8910ef724e09ed740aacc32131ff5fbbdc0a7f85
Jitse De Smet <jitse@reedonk8.be>
2025-09-26 11:02:17 +0200

    TESTS: add more benchmarks

--- engines/parser-sparql-1-1/test/query.bench.ts
+++ engines/parser-sparql-1-1/test/query.bench.ts
@@ -1,17 +1,36 @@
+import { positiveTest } from '@traqula/test-utils';
 import { Parser as SparqlJSparser } from 'sparqljs';
 import { describe, bench } from 'vitest';
 import { Parser as TraqulaParqer } from '../lib/index.js';
 import { queryLargeObjectList } from './heatmap.js';
 
 describe('query, exclude construction', () => {
-  const traqulaParqer = new TraqulaParqer();
+  const traqulaParser = new TraqulaParqer();
   const sparqlJSparser = new SparqlJSparser();
   const query = queryLargeObjectList;
 
-  bench('traqula parse', () => {
-    traqulaParqer.parse(query);
+  describe('large objectList', () => {
+    bench('traqula parse', () => {
+      traqulaParser.parse(query);
+    });
+    bench('sparqljs', () => {
+      sparqlJSparser.parse(query);
+    });
   });
-  bench('sparqljs', () => {
-    sparqlJSparser.parse(query);
+
+  describe('general queries', async() => {
+    const allQueries = await Promise.all([ ...positiveTest('sparql-1-1') ]
+      .map(x => x.statics().then(x => x.query)));
+
+    bench('traqula parse', () => {
+      for (const query of allQueries) {
+        traqulaParser.parse(query);
+      }
+    });
+    bench('sparqljs', () => {
+      for (const query of allQueries) {
+        sparqlJSparser.parse(query);
+      }
+    });
   });
 });

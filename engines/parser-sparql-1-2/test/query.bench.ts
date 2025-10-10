c206d40765425ae2935d040825d1cb933f6783c2
Jitse De Smet <jitse@reedonk8.be>
2025-10-10 15:26:52 +0200

    add more benchmarks to compare to sparqlAlgebra and sparqlJS

--- engines/parser-sparql-1-2/test/query.bench.ts
+++ engines/parser-sparql-1-2/test/query.bench.ts
@@ -2,27 +2,16 @@ import { positiveTest } from '@traqula/test-utils';
 import { Parser as SparqlJSparser } from 'sparqljs';
 import { describe, bench } from 'vitest';
 import { Parser as TraqulaParser } from '../lib/index.js';
-import { queryLargeObjectList } from './heatmap.js';
 
-describe('query 1.1, exclude construction', () => {
+describe('query 1.2, exclude construction', () => {
   const traqulaParser = new TraqulaParser();
   const sparqlJSparser = new SparqlJSparser();
-  const query = queryLargeObjectList;
-
-  describe('large objectList', () => {
-    bench('traqula parse', () => {
-      traqulaParser.parse(query);
-    });
-    bench('sparqljs', () => {
-      sparqlJSparser.parse(query);
-    });
-  });
 
   describe('general queries', async() => {
     const allQueries = await Promise.all([ ...positiveTest('sparql-1-1') ]
       .map(x => x.statics().then(x => x.query)));
 
-    bench('traqula parse 1.1', () => {
+    bench('traqula parse 1.2', () => {
       for (const query of allQueries) {
         traqulaParser.parse(query);
       }
